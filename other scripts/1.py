1b4c9b94e2bd1bbbe7e77fd1cfb056f4360b662b
Koryy <iluwka13@gmail.com>
2022-04-01 22:33:22 +0700

    second commit

--- other scripts/1.py
+++
@@ -1,9 +0,0 @@
-import win32api
-import win32gui
-import win32ui
-import time
-
-time.sleep(3)
-hWnd_manual = win32gui.GetForegroundWindow()
-hWnd = win32gui.FindWindow(None, 'EVE - AnSiri Senpai')
-print('manual', hWnd_manual, '\nauto', hWnd) # 105711036
